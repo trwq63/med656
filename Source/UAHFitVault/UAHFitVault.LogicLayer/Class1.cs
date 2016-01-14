36cf0f2dec99018fed718f04de84cdbf3665529a
duggajj <duggajj@yahoo.com>
2016-01-13 20:56:42 -0600

    Created Logic Layer project and include git files in solution.

---
+++ Source/UAHFitVault/UAHFitVault.LogicLayer/Class1.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace UAHFitVault.LogicLayer
+{
+    public class Class1
+    {
+    }
+}
