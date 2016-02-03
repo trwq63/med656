2061a3c682dff40a212996ac98e7c288ee75e643
duggajj <duggajj@yahoo.com>
2016-02-02 21:31:25 -0600

    Fix problems with seed class.  Also commented out calling the seed methods.  This only needs to be run when needed not everytime the application launches.

---
+++ Source/UAHFitVault/UAHFitVault/App_Start/AppDbSeedData.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections.Generic;
+using System.Data.Entity;
+using UAHFitVault.Models;
+
+namespace UAHFitVault.App_Start
+{
+    public class AppDbSeedData : DropCreateDatabaseIfModelChanges<ApplicationDbContext>
+    {
+        protected override void Seed(ApplicationDbContext context) {
+            //base.Seed(context);
+            GetUsers().ForEach(u => context.Users.Add(u));
+        }
+
+        private static List<ApplicationUser> GetUsers() {
+            string userId = "95eee4b8 - 615f - 4777 - 932e-7e3f50db8233";
+           // string roleId = "6402f1aa-7ae0-4f98-b2a8-dea595d3b31f";
+
+            return new List<ApplicationUser>
+            {
+                new ApplicationUser
+                {
+                    //Roles = new List<IdentityUserRole>() {
+                    //    new IdentityUserRole() {
+                    //        RoleId = roleId,
+                    //        UserId = userId
+                    //    }
+                    //},
+                    Id = userId,                    
+                    UserName = "fry@futurama.com",
+                    Status = 1,
+                    PasswordHash = ""
+                },
+            };
+        }
+    }
+}
