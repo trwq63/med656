85faf50239437f64260cc2504c8ae4086e6b982a
rideng <glr0001@uah.edu>
2016-03-05 01:06:11 -0600

    ExperimentAdmin-MainViews
    Adding some of the main views for the experiment administrator

--- Source/UAHFitVault/UAHFitVault/Controllers/ExperimentController.cs
+++ Source/UAHFitVault/UAHFitVault/Controllers/ExperimentController.cs
@@ -4,6 +4,8 @@ using System.Linq;
 using System.Web;
 using System.Web.Mvc;
 using UAHFitVault.Helpers;
+using UAHFitVault.Models;
+using UAHFitVault.LogicLayer.Enums;
 
 namespace UAHFitVault.Controllers
 {
@@ -13,6 +15,44 @@ namespace UAHFitVault.Controllers
         // GET: Experiment
         public ActionResult Index()
         {
+            return View();
+        }
+
+
+        /// <summary>
+        /// Displays the view for an experiment administrator to create an experiment
+        /// </summary>
+        /// <returns></returns>
+        public ActionResult CreateExperiment ()
+        {
+            return View();
+        }
+
+        /// <summary>
+        /// This handles the processing after the user hits the submit button on the create experiment view
+        /// </summary>
+        /// <returns></returns>
+        [HttpPost]
+        public ActionResult CreateExperiment (CreateExperimentViewModel model)
+        {
+            /*  Code doesn't work
+            int maxGenders = Enum.GetNames(typeof(PatientGender)).Length;
+
+            for (int i = 0; i < maxGenders; i++)
+            {
+                if (model.genders[i] == true)
+                {
+
+                }
+                else {
+                    ModelState.AddModelError("", "ERROR: No race was checked");
+                }
+            }
+            */
+            if (!ModelState.IsValid)
+            {
+                return View();
+            }
             return View();
         }
     }
---
+++ Source/UAHFitVault/UAHFitVault/Models/ExperimentAdminViewModels.cs
@@ -0,0 +1,49 @@
+﻿using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using UAHFitVault.LogicLayer.Enums;
+using System;
+
+namespace UAHFitVault.Models
+{
+    /// <summary>
+    /// Model for creating experiments
+    /// </summary>
+    public class CreateExperimentViewModel
+    {
+        /// <summary>
+        /// The name of the experiment
+        /// </summary>
+        [Required]
+        [Display(Name = "Experiment Name")]
+        public string ExperimentName { get; set; }
+
+        [Display(Name = "Start of Age Range")]
+        public int ageRangeStart { get; set; }
+
+        [Display(Name = "End of Age Range")]
+        public int ageRangeEnd { get; set; }
+
+        [Display(Name = "Start of Weight Range (pounds)")]
+        public int weightRangeBegin { get; set; }
+
+        [Display(Name = "End of Weight Range (pounds)")]
+        public int weightRangeEnd { get; set; }
+
+        [Display(Name = "Start of Height Range (inches)")]
+        public int heightRangeBegin { get; set; }
+
+        [Display(Name = "End of Height Range (inches)")]
+        public int heightRangeEnd { get; set; }
+
+        public List<bool> races; // Races
+
+        public List<bool> genders; // Sexes
+
+        public List<int> locations; // Select location
+
+        public List<int> ethnicities; // Ethnicities
+
+        public List<int> activities; // Activity tags
+        
+    }
+}
