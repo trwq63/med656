603314662eecdf7de4f06d85adecc05bb19d178a
duggajj <duggajj@yahoo.com>
2016-03-13 20:36:24 -0500

    Created all microsoft band repository classes

---
+++ Source/UAHFitVault/UAHFitVault.Database/Repositories/MicrosoftBandRepositories/IMSBandGyroscopeRepository.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections.Generic;
+using UAHFitVault.Database.Entities;
+using UAHFitVault.Database.Infrastructure;
+
+namespace UAHFitVault.Database.Repositories.MicrosoftBandRepositories
+{
+    /// <summary>
+    /// Interface to implement Microsoft Band Gyroscope repository.
+    /// </summary>
+    public interface IMSBandGyroscopeRepository : IRepository<MSBandGryoscope>
+    {
+        /// <summary>
+        /// Get a Microsoft Band Gyroscope records using patient data record id
+        /// </summary>
+        /// <param name="id">Id of the patient data record</param>
+        /// <returns></returns>
+        IEnumerable<MSBandGryoscope> GetMSBandGyroscopeByPatientDataId(string id);
+    }
+}
---
+++ Source/UAHFitVault/UAHFitVault.Database/Repositories/MicrosoftBandRepositories/MSBandGyroscopeRepository.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using UAHFitVault.Database.Entities;
+using UAHFitVault.Database.Infrastructure;
+
+namespace UAHFitVault.Database.Repositories.MicrosoftBandRepositories
+{
+    /// <summary>
+    /// Implementation of the repository base class for the MSBandGryoscope model
+    /// </summary>
+    public class MSBandGyroscopeRepository : RepositoryBase<MSBandGryoscope>, IMSBandGyroscopeRepository
+    {
+        #region Public Constructor
+        /// <summary>
+        /// Default constructor
+        /// </summary>
+        /// <param name="dbFactory">Interface for dbFactory</param>
+        public MSBandGyroscopeRepository(IDbFactory dbFactory)
+            : base(dbFactory) { }
+
+        #endregion
+
+        #region Repository Public Functions
+
+        /// <summary>
+        /// Get a Microsoft Band Gyroscope records using patient data record id
+        /// </summary>
+        /// <param name="id">Id of the patient data record</param>
+        /// <returns></returns>
+        public IEnumerable<MSBandGryoscope> GetMSBandGyroscopeByPatientDataId(string id) {
+            IEnumerable<MSBandGryoscope> msBandGyroscope = this.DbContext.MSBandGyroscrope.Where(p => p.PatientDataId == id);                                                    
+
+            return msBandGyroscope;
+        }
+
+        #endregion
+    }
+}
