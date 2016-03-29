bc4ce52e77d26e48d4a8be9e779edb87441c788f
duggajj <duggajj@yahoo.com>
2016-03-28 23:59:14 -0500

    Created activity model for reading activities from view.

---
+++ Source/UAHFitVault/UAHFitVault/Models/ActivityModel.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace UAHFitVault.Models
+{
+    /// <summary>
+    /// Model for each activity to be tagged to data being uploaded.
+    /// </summary>
+    public class ActivityModel
+    {
+        #region Public Properties
+        /// <summary>
+        /// Name of the activity type performed for the data set.
+        /// </summary>
+        public string ActivityType { get; set; }
+
+        /// <summary>
+        /// Start time of the activity.
+        /// </summary>
+        public DateTime StartDate { get; set; }
+
+        /// <summary>
+        /// End time of the activity.
+        /// </summary>
+        public DateTime EndDate { get; set; }
+
+        #endregion
+    }
+}
