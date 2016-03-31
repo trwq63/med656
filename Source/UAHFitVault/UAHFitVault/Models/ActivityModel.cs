e03ab0ff31b2f3deff044018fc0db268c6e67355
duggajj <duggajj@yahoo.com>
2016-03-31 02:27:21 -0500

    Activities now submit correctly to controller

--- Source/UAHFitVault/UAHFitVault/Models/ActivityModel.cs
+++ Source/UAHFitVault/UAHFitVault/Models/ActivityModel.cs
@@ -7,6 +7,20 @@ namespace UAHFitVault.Models
     /// </summary>
     public class ActivityModel
     {
+        #region Private Members
+
+        /// <summary>
+        /// Start time of the activity.
+        /// </summary>
+        private DateTime _startTime;
+
+        /// <summary>
+        /// End time of the activity.
+        /// </summary>
+        private DateTime _endTime;
+
+        #endregion
+
         #region Public Properties
         /// <summary>
         /// Name of the activity type performed for the data set.
@@ -14,15 +28,35 @@ namespace UAHFitVault.Models
         public string ActivityType { get; set; }
 
         /// <summary>
+        /// Date the activity took place on.
+        /// </summary>
+        public DateTime ActivityDate { get; set; }
+
+        /// <summary>
         /// Start time of the activity.
         /// </summary>
-        public DateTime StartDate { get; set; }
+        public DateTime StartTime {
+            get {
+                DateTime startTime = new DateTime(ActivityDate.Year, ActivityDate.Month, ActivityDate.Day, _startTime.Hour, _startTime.Minute, _startTime.Second);
+                return startTime;
+            }
+            set {
+                _startTime = value;
+            }
+        }
 
         /// <summary>
         /// End time of the activity.
         /// </summary>
-        public DateTime EndDate { get; set; }
-
-        #endregion
-    }
+        public DateTime EndTime {
+            get {
+                DateTime endTime = new DateTime(ActivityDate.Year, ActivityDate.Month, ActivityDate.Day, _endTime.Hour, _endTime.Minute, _endTime.Second);
+                return endTime;
+            }
+            set {
+                _endTime = value;
+            }
+        }
+            #endregion
+        }
 }
