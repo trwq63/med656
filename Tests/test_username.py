ad90f3c1c4666276dbac8a204acf2007ddb9d86a
Ryan Wilkins <ryan.wilkins@adtran.com>
2016-03-31 15:29:38 -0500

    added documentation

--- Tests/test_username.py
+++ Tests/test_username.py
@@ -8,13 +8,37 @@ web_sess = WebUI()
 
 def test_username_cannot_be_copied(logoff):
     """
-    This test creates a new user, tries to create another user with the same name, then creates third unique user
-    Validatin is done on all three steps. This verifies that the username must be unique.
-    Note: Need a step added to check that usernames cannot be changed to a copy
+    **Requirements:**
 
-    :param logoff: Makes sure the session is in a logged off state before the test starts.
-    :return:
+    - 3.1.1.2: Usernames must be unique
+    - 3.1.1.2.1: Users cannot change username to an already existing username
+
+    **Pre Conditions:**
+
+    - logoff fixture
+
+    **Input:**
+
+    - user = 'copy'
+    - pwd = 'P@ssword10'
+    - email = 'copy@copy.com'
+    - first_name = 'Fry'
+    - last_name = 'Phillip'
+    - address = '304 Wherever Street, New New York City, New New York'
+    - phone_number = '123-456-7890'
+    - user_2 = 'copy1'
+    - email_2 = 'copy2@copy.com'
+
+    ==============================  ====================  =============
+    Steps                           Expected Result       Actual Result
+    ==============================  ====================  =============
+    Request physician account       No errors
+    Request same physician account  Username taken error
+    Request different account       No errors
+    ==============================  ====================  =============
     """
+    print('Starting')
+
     user = 'copy'
     pwd = 'P@ssword10'
     email = 'copy@copy.com'
@@ -22,15 +46,38 @@ def test_username_cannot_be_copied(logoff):
     last_name = 'Phillip'
     address = '304 Wherever Street, New New York City, New New York'
     phone_number = '123-456-7890'
+    user_2 = 'copy1'
+    email_2 = 'copy2@copy.com'
 
     # first use of user copy
+    print('Requesting account for user ', user,
+          'pwd: ', pwd,
+          'email: ', email,
+          'first_name: ', first_name,
+          'last_name: ', last_name,
+          'address: ', address,
+          'phone_number: ', phone_number)
     web_sess.request_account('Physician', user, pwd, email, first_name, last_name, address, phone_number)
     assert web_sess.check_request_account()
 
     # second use of user copy
+    print('Requesting account for user ', user,
+      'pwd: ', pwd,
+      'email: ', email,
+      'first_name: ', first_name,
+      'last_name: ', last_name,
+      'address: ', address,
+      'phone_number: ', phone_number)
     web_sess.request_account('Physician', user, pwd, email, first_name, last_name, address, phone_number)
     assert 'Name {} is already taken.'.format(user) in web_sess.get_page()
 
     # first use of user copy1
-    web_sess.request_account('Physician', 'copy1', pwd, 'copy1@copy.com', first_name, last_name, address, phone_number)
+    print('Requesting account for user ', user_2,
+          'pwd: ', pwd,
+          'email: ', email_2,
+          'first_name: ', first_name,
+          'last_name: ', last_name,
+          'address: ', address,
+          'phone_number: ', phone_number)
+    web_sess.request_account('Physician', user_2, pwd, email_2, first_name, last_name, address, phone_number)
     assert web_sess.check_request_account()
