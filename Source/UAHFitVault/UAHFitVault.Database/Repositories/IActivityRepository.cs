89c32f1f6f42e480a12d39ee4b5673995bf106a5
duggajj <duggajj@yahoo.com>
2016-03-31 22:29:55 -0500

    user can assign activities to uploaded data.  build in some error checking on each activity prior to inserting it to the database.

---
+++ Source/UAHFitVault/UAHFitVault.Database/Repositories/IActivityRepository.cs
@@ -0,0 +1,18 @@
+﻿using UAHFitVault.Database.Entities;
+using UAHFitVault.Database.Infrastructure;
+
+namespace UAHFitVault.Database.Repositories
+{
+    /// <summary>
+    /// Interface for the Activity repository.
+    /// </summary>
+    public interface IActivityRepository : IRepository<Activity>
+    {
+        /// <summary>
+        /// Get an activity using its ID.
+        /// </summary>
+        /// <param name="id">ID of the activity</param>
+        /// <returns>Account Request</returns>
+        Activity GetActivity(int id);
+    }
+}
