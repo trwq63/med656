6532cd4c9c74dab396ebae32cb5541a89e582d3a
trwq63 <ryan.wilkins@adtran.com>
2016-04-09 10:25:56 -0700

    wrote test

--- Tests/test_security.py
+++ Tests/test_security.py
@@ -15,85 +15,65 @@ def test_physician_to_patient(logoff):
 
     **Input:**
 
-    - user = ''
-    - pwd = ''
+    - physician = 'testPhysician3'
+    - phys_pass = 'P@ssword10'
+    - phys_email = 'testphysician3@uah.com'
+    - phys_add = 'here'
+    - phys_fname = 'test'
+    - phys_lname = 'physicianthree'
+    - phys_phone = '123-456-7890'
+    - patient = 'testPatient5'
+    - pat_pass = 'P@ssword10'
+    - bday = 'April 1, 1987'
+    - loc = 'Alabama'
+    - wght = '123'
+    - hght = '123'
+    - gen = 'male'
+    - race = 'white'
+    - eth = 'non_hispanic'
+    - test_physician = 'testPhysician'
+    - test_phys_pass = 'P@ssword10'
 
-    ===============  =================  =============
-    Steps            Expected Result    Actual Result
-    ===============  =================  =============
-    login            User can login
-    ===============  =================  =============
+    ====================================  ===================  =============
+    Steps                                 Expected Result      Actual Result
+    ====================================  ===================  =============
+    create physician                      no errors
+    create patient for physician          no errors
+    login as testPhysician                login success
+    check for patient in management page  patient not listed
+    ====================================  ===================  =============
     """
     print('Starting')
 
-    user = ''
-    pwd = ''
+    physician = 'testPhysician3'
+    phys_pass = 'P@ssword10'
+    phys_email = 'testphysician3@uah.com'
+    phys_add = 'here'
+    phys_fname = 'test'
+    phys_lname = 'physicianthree'
+    phys_phone = '123-456-7890'
+    patient = 'testPatient5'
+    pat_pass = 'P@ssword10'
+    bday = 'April 1, 1987'
+    loc = 'Alabama'
+    wght = '123'
+    hght = '123'
+    gen = 'male'
+    race = 'white'
+    eth = 'non_hispanic'
+    test_physician = 'testPhysician'
+    test_phys_pass = 'P@ssword10'
 
-    print('Logging in as ', user)
-    web_sess.login(user, pwd)
-    ps = web_sess.driver.get('')
-    assert 'Restricted' in ps
-
-
-def test_patient_cannot_create_patient(logoff):
-    """
-    **Requirements:**
-
-    - 3.1.1.1.2.5: Only a physician can create a patient
-
-    **Pre Conditions:**
-
-    - logoff fixture
-
-    **Input:**
-
-    - user = ''
-    - pwd = ''
-
-
-    ===============  =================  =============
-    Steps            Expected Result    Actual Result
-    ===============  =================  =============
-    login            User can login
-    ===============  =================  =============
-    """
-    print('Starting')
-
-    user = ''
-    pwd = ''
-
-    print('Logging in as ', user)
-    web_sess.login(user, pwd)
-    assert False
-
-
-def test_exp_admin_cannot_create_patient():
-    """
-    **Requirements:**
-
-    - 3.1.1.1.2.5: Only a physician can create a patient
-
-    **Pre Conditions:**
-
-    - logoff fixture
-
-    **Input:**
-
-    - user = ''
-    - pwd = ''
-
-
-    ===============  =================  =============
-    Steps            Expected Result    Actual Result
-    ===============  =================  =============
-    login            User can login
-    ===============  =================  =============
-    """
-    print('Starting')
-
-    user = ''
-    pwd = ''
-
-    print('Logging in as ', user)
-    web_sess.login(user, pwd)
-    assert False
+    print('Creating physician ', physician)
+    web_sess.request_account('Physician', physician, phys_pass, phys_email, phys_fname, phys_lname, phys_add, phys_phone)
+    print('Apprpving physician', physician)
+    web_sess.approve_account('{} {}'.format(phys_fname, phys_lname))
+    print('Creating patient', patient, ' for physician ', physician)
+    web_sess.logoff()
+    web_sess.create_patient(physician, phys_pass, patient, pat_pass, bday, loc, wght, hght, gen, race, eth)
+    print('Logging in as', test_physician)
+    web_sess.logoff()
+    web_sess.login(test_physician, test_phys_pass)
+    print('Checking for patient', patient)
+    text = web_sess.driver.find_element_by_css_selector('div[class="card"]').get_attribute('innerHTML')
+    assert '{} {}'.format(phys_fname, phys_lname) not in text
