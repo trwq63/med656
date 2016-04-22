25a440346f10bde0ef808eac1686fd484165c204
duggajj <duggajj@yahoo.com>
2016-04-22 01:05:07 -0500

    Add installation guide and sql database setup scripts.

---
+++ SQL Setup/medical_devices_script.sql
@@ -0,0 +1,5 @@
+SET IDENTITY_INSERT [dbo].[MedicalDevices] ON
+INSERT INTO [dbo].[MedicalDevices] ([Id], [Name]) VALUES (1, N'Zephyr')
+INSERT INTO [dbo].[MedicalDevices] ([Id], [Name]) VALUES (2, N'BasisPeak')
+INSERT INTO [dbo].[MedicalDevices] ([Id], [Name]) VALUES (3, N'Microsoft Band')
+SET IDENTITY_INSERT [dbo].[MedicalDevices] OFF
