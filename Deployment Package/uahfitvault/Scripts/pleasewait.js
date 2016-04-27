d4e574f10d018a721efd5c99858872220a442b7e
duggajj <duggajj@yahoo.com>
2016-04-26 23:28:23 -0500

    commit initial deployment package

---
+++ Deployment Package/uahfitvault/Scripts/pleasewait.js
@@ -0,0 +1,22 @@
+﻿function StartWaiting(spinner) {
+    if (arguments.length == 0) {
+        spinner = 1;
+    }
+    if (spinner < 0 || spinner > 8) {
+        spinner = 1;
+    }
+    $.blockUI({
+        css: {
+            border: 'none',
+            padding: '15px',
+            backgroudColor: 'rgba(215, 44, 44, 0',
+            opacity: 1,
+            color: '#fff'
+        },
+        message: '<div class="loader' + spinner + '">Loading...</div>'
+    });
+}
+
+function StopWaiting() {
+    $.unblockUI();
+}
